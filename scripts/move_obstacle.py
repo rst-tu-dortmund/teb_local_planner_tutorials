9f0f41cfc6776ebe166471539d87c9019d9cea9a
Franz Albers <Franz.Albers@tu-dortmund.de>
2017-09-14 11:08:34 +0200

    Added scripts and maps

---
+++ scripts/move_obstacle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import rospy, math, random
+from geometry_msgs.msg import Twist, Pose
+from nav_msgs.msg import Odometry
+
+# Define parameters, if parameter server provides no definitions
+# Lower bound of the obstacle movement in y-direction
+if rospy.has_param("pos_lb"):
+  pos_lb = rospy.get_param("pos_lb")
+else:
+  pos_lb = -2.5
+
+# Upper bound of the obstacle movement in y-direction
+if rospy.has_param("pos_ub"):
+  pos_ub = rospy.get_param("pos_ub")
+else:
+  pos_ub = 2.5
+
+# Define global Twist message which is modified in the callback_base_pose_ground_truth and published in the main loop
+Twist_msg = Twist()
+
+
+# This function manages turnarounds and new (possibly random) velocities of the object
+def callback_base_pose_ground_truth(base_pose_ground_truth):
+  # Define random velocity if no specific velocity is defined in the parameter server
+  if rospy.has_param("velocity"):
+  	vel = rospy.get_param("velocity")
+  else:
+  	vel = random.uniform(0.1, 1.0)
+
+  # Turn in y-direction
+  if base_pose_ground_truth.pose.pose.position.y >= pos_ub:
+    Twist_msg.linear.y = -vel
+    print(vel)
+  if base_pose_ground_truth.pose.pose.position.y <= pos_lb:
+    Twist_msg.linear.y = vel
+    print(vel)
+
+
+# This function initializes the mover node and publishes continously a Twist message
+def move_object():
+  rospy.init_node("Mover")
+  r = rospy.Rate(10)
+  pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+  sub = rospy.Subscriber('base_pose_ground_truth', Odometry, callback_base_pose_ground_truth)
+  start_t = rospy.get_time()
+
+  # initialize movement with random direction
+  if rospy.has_param("velocity"):
+  	vel = rospy.get_param("velocity")
+  else:
+  	vel = random.uniform(0.1, 1.0)
+  Twist_msg.linear.y = float(random.choice(['-1', '1'])) * vel
+
+  # publish movement command continuously
+  while not rospy.is_shutdown():
+    pub.publish(Twist_msg)
+    r.sleep()
+
+
+if __name__ == '__main__': 
+  try:
+    move_object()
+  except rospy.ROSInterruptException:
+    pass
---
+++ scripts/visualize_obstacle_velocity_profile.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+
+# This small script subscribes to the FeedbackMsg message of teb_local_planner
+# and plots the current velocity.
+# publish_feedback must be turned on such that the planner publishes this information.
+# Author: christoph.roesmann@tu-dortmund.de
+
+import rospy, math
+from teb_local_planner.msg import FeedbackMsg, TrajectoryMsg, TrajectoryPointMsg, ObstacleMsg
+from geometry_msgs.msg import PolygonStamped, Point32, Twist
+import numpy as np
+import matplotlib.pyplot as plotter
+
+def twist_callback(data):
+  global trajectory_gt
+  global start_t
+
+  if (start_t == 0):
+    start_t = rospy.Time.now() # cmd_vel doesn't provide any time information, therefore measure it
+
+  point = TrajectoryPointMsg()
+  point.time_from_start = (rospy.Time.now()-start_t)
+  point.velocity.linear.x = data.linear.x
+  point.velocity.linear.y = data.linear.y
+  point.velocity.angular.z = data.angular.z
+
+  trajectory_gt.append(point)
+
+
+def obstacleMsg_callback(data):
+  global trajectory_est
+  global start_t
+
+  if (start_t == 0):
+    start_t = rospy.Time.now()
+
+  point = TrajectoryPointMsg()
+  point.time_from_start = (rospy.Time.now()-start_t)
+  if(len(data.velocities) > 0):
+    point.velocity.linear.x = data.velocities[0].twist.linear.x
+    point.velocity.linear.y = data.velocities[0].twist.linear.y
+    point.velocity.angular.z = data.velocities[0].twist.angular.z
+
+    trajectory_est.append(point)
+
+
+def plot_velocity_profiles(fig, ax_vx, ax_vy, t_gt, v_x_gt, v_y_gt, omega_gt, t_est, v_x_est, v_y_est, omega_est):
+  ax_vx.cla()
+  ax_vx.grid()
+  ax_vx.set_ylabel('Trans. velocity (x) [m/s]')
+  ax_vx.plot(t_gt, v_x_gt, '-bx', label='Ground Truth')
+  ax_vx.plot(t_est, v_x_est, '-rx', label='Estimate')
+  ax_vx.legend(loc='upper left')
+  ax_vy.cla()
+  ax_vy.grid()
+  ax_vy.set_ylabel('Trans. velocity (y) [m/s]')
+  ax_vy.plot(t_gt, v_y_gt, '-bx', label='Ground Truth')
+  ax_vy.plot(t_est, v_y_est, '-rx', label='Estimate')
+  ax_vy.set_xlabel('Time [s]')
+  fig.canvas.draw()
+
+  
+def velocity_plotter():
+  global trajectory_gt
+  global trajectory_est
+  global start_t
+
+  rospy.init_node("visualize_obstacle_velocity_profile", anonymous=True)
+  
+  topic_name_ground_truth_vel = "cmd_vel"
+  rospy.Subscriber(topic_name_ground_truth_vel, Twist, twist_callback, queue_size = 1)
+  topic_name_estimated_vel = "/costmap_obstacles"
+  rospy.Subscriber(topic_name_estimated_vel, ObstacleMsg, obstacleMsg_callback, queue_size = 1)
+
+  rospy.loginfo("Visualizing ground truth velocity profile published on '%s'.", topic_name_ground_truth_vel)
+  rospy.loginfo("Visualizing estimated velocity profile published on '%s'.", topic_name_estimated_vel)
+    
+  # two subplots sharing the same t axis
+  fig, (ax_vx, ax_vy) = plotter.subplots(2, sharex=True)
+  plotter.ion()
+  plotter.show()
+  
+
+  r = rospy.Rate(2) # define rate here
+  while not rospy.is_shutdown():
+    
+    t_gt = []
+    v_x_gt = []
+    v_y_gt = []
+    omega_gt = []
+
+    t_est = []
+    v_x_est = []
+    v_y_est = []
+    omega_est = []
+    
+    for point in trajectory_gt:
+      t_gt.append(point.time_from_start.to_sec())
+      v_x_gt.append(point.velocity.linear.x)
+      v_y_gt.append(point.velocity.linear.y)
+      omega_gt.append(point.velocity.angular.z)
+
+    for point in trajectory_est:
+      t_est.append(point.time_from_start.to_sec())
+      v_x_est.append(point.velocity.linear.x)
+      v_y_est.append(point.velocity.linear.y)
+      omega_est.append(point.velocity.angular.z)
+
+          
+    plot_velocity_profiles(fig, ax_vx, ax_vy, np.asarray(t_gt), np.asarray(v_x_gt), np.asarray(v_y_gt), np.asarray(omega_gt), 
+                            np.asarray(t_est), np.asarray(v_x_est), np.asarray(v_y_est), np.asarray(omega_est))
+        
+    r.sleep()
+
+if __name__ == '__main__': 
+  try:
+    trajectory_gt = []
+    trajectory_est = []
+    start_t = 0
+    velocity_plotter()
+  except rospy.ROSInterruptException:
+    pass
+#  finally:
+#    plotter.savefig('/home/albers/Desktop/velocity_plot.pdf', bbox_inches='tight')
+
