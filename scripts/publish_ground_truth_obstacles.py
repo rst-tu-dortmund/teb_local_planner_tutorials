1623fb4e16f8420c9a6743fe02499cc7ce4bcdb4
Christoph Rösmann <christoph.roesmann@tu-dortmund.de>
2017-09-15 23:34:26 +0200

    ground truth obstacle python script updated according to the new ObstacleArrayMsg

--- scripts/publish_ground_truth_obstacles.py
+++ scripts/publish_ground_truth_obstacles.py
@@ -4,46 +4,43 @@ import rospy, math, random, re
 from tf.transformations import quaternion_from_euler
 from geometry_msgs.msg import Twist, PolygonStamped, Quaternion, QuaternionStamped, TwistWithCovariance, Point32
 from nav_msgs.msg import Odometry
-from teb_local_planner.msg import ObstacleMsg
+from costmap_converter.msg import ObstacleArrayMsg, ObstacleMsg
 
 # global variables
-Obstacle_msg = ObstacleMsg()
-obst_pub = rospy.Publisher('/robot_0/move_base/TebLocalPlannerROS/obstacles', ObstacleMsg, queue_size = 10)
+obstacles_msg = ObstacleArrayMsg()
+obst_pub = rospy.Publisher('/robot_0/move_base/TebLocalPlannerROS/obstacles', ObstacleArrayMsg, queue_size = 10)
 
 def callback_base_pose_ground_truth(base_pose_ground_truth, obst_id):
-  # Search for obstacle id and update fields, if found
-  # try to find the current id
-  try:
-    i = Obstacle_msg.ids.index(obst_id)
-  # Obstacle with current id not published so far
-  except ValueError:
-    i = -1 # -1 is the last element
-    Obstacle_msg.ids.append(obst_id)
-    Obstacle_msg.obstacles.append(PolygonStamped())
-    Obstacle_msg.orientations.append(QuaternionStamped())
-    Obstacle_msg.velocities.append(TwistWithCovariance())
+  # Search for obstacle id and update fields if found
+  i = -1 # -1 is the last element
+  for idx, obst in enumerate(obstacles_msg.obstacles):
+    if obst.id == obst_id:
+      i = idx
+   
+  if i == -1:
+    obstacle_msg = ObstacleMsg()
+    obstacle_msg.id = obst_id
+    obstacles_msg.obstacles.append(obstacle_msg)
 
   # HEADER
-  Obstacle_msg.header.stamp = rospy.Time.now()
-  Obstacle_msg.header.frame_id = 'map' # TODO? Seems to works this way..
+  obstacles_msg.header.stamp = rospy.Time.now()
+  obstacles_msg.header.frame_id = 'map'
 
   # OBSTACLES
-  Obstacle_msg.obstacles[i].header.stamp = Obstacle_msg.header.stamp
-  Obstacle_msg.obstacles[i].header.frame_id = Obstacle_msg.header.frame_id
-  Obstacle_msg.obstacles[i].polygon.points = [Point32()] # currently only point obstacles
-  Obstacle_msg.obstacles[i].polygon.points[0].x = base_pose_ground_truth.pose.pose.position.x
-  Obstacle_msg.obstacles[i].polygon.points[0].y = base_pose_ground_truth.pose.pose.position.y
-  Obstacle_msg.obstacles[i].polygon.points[0].z = base_pose_ground_truth.pose.pose.position.z
+  obstacles_msg.obstacles[i].header.stamp = obstacles_msg.header.stamp
+  obstacles_msg.obstacles[i].header.frame_id = obstacles_msg.header.frame_id
+  obstacles_msg.obstacles[i].polygon.points = [Point32()] # currently only point obstacles
+  obstacles_msg.obstacles[i].polygon.points[0].x = base_pose_ground_truth.pose.pose.position.x
+  obstacles_msg.obstacles[i].polygon.points[0].y = base_pose_ground_truth.pose.pose.position.y
+  obstacles_msg.obstacles[i].polygon.points[0].z = base_pose_ground_truth.pose.pose.position.z
 
   # ORIENTATIONS
-  Obstacle_msg.orientations[i].header.stamp = Obstacle_msg.header.stamp
-  Obstacle_msg.orientations[i].header.frame_id = Obstacle_msg.header.frame_id
   yaw = math.atan2(base_pose_ground_truth.twist.twist.linear.y, base_pose_ground_truth.twist.twist.linear.x)
   quat = quaternion_from_euler(0.0, 0.0, yaw) # roll, pitch, yaw in radians
-  Obstacle_msg.orientations[i].quaternion = Quaternion(*quat.tolist())
+  obstacles_msg.obstacles[i].orientation = Quaternion(*quat.tolist())
 
   # VELOCITIES
-  Obstacle_msg.velocities[i] = base_pose_ground_truth.twist
+  obstacles_msg.obstacles[i].velocities = base_pose_ground_truth.twist
 
 
 def init_subscribers():
@@ -63,7 +60,7 @@ def init_subscribers():
 
   while not rospy.is_shutdown():
     # Publish Obstacle_msg
-    obst_pub.publish(Obstacle_msg)
+    obst_pub.publish(obstacles_msg)
     r.sleep()
 
 
